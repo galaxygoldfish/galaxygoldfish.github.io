240093f694ccb336a6d96f2ee092ee7da63fa936
galaxygoldfish <sebastianhriscu25@gmail.com>
2025-03-24 20:50:36 -0700

    Attempt at making responsive for mobile

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// Blurring menu background on scroll
 window.addEventListener("scroll", function() {
     const navbar = document.getElementById("navbar");
     if (window.scrollY > 70) {
-        navbar.classList.add("backdrop-blur-lg", "bg-white/30", "shadow-xs", "pb-7", "pt-7");
+        navbar.classList.add("backdrop-blur-lg", "bg-white/30", "shadow-sm", "pb-7", "pt-7");
     } else {
-        navbar.classList.remove("backdrop-blur-lg", "bg-white/30", "shadow-xs", "pb-7", "pt-7");
+        navbar.classList.remove("backdrop-blur-lg", "bg-white/30", "shadow-sm", "pb-7", "pt-7");
     }
 });
