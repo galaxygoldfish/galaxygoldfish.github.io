90d32788d710f17159de468a85d1b6cbeb5902f6
galaxygoldfish <sebastianhriscu25@gmail.com>
2025-06-19 19:32:13 -0700

    update

--- script.js
+++
@@ -1,9 +0,0 @@
-// Blurring menu background on scroll
-window.addEventListener("scroll", function() {
-    const navbar = document.getElementById("navbar");
-    if (window.scrollY > 70) {
-        navbar.classList.add("backdrop-blur-lg", "bg-white/30", "shadow-sm", "pb-7", "pt-7");
-    } else {
-        navbar.classList.remove("backdrop-blur-lg", "bg-white/30", "shadow-sm", "pb-7", "pt-7");
-    }
-});
